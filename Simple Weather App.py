d5a1b7b533d5895343ee431680e619aa8af1a5a1
Soumyo Roy <soumyo78@yahoo.com>
2020-05-13 16:49:00 +0530

    Initial Commit

---
+++ Simple Weather App.py
@@ -0,0 +1,15 @@
+from tkinter import *
+
+root = Tk()
+root.title("Simple Weather")
+root.geometry("400x400")
+root.iconphoto(True, PhotoImage(file="/home/soumyo/PycharmProjects/Simple Weather Application/icon"))
+
+
+
+
+
+
+
+
+root.mainloop()
