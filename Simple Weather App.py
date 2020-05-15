a48c78f7c48d48bdf77be15f9b50f3eeecc90ffb
Soumyo Roy <soumyo78@yahoo.com>
2020-05-15 18:42:55 +0530

    Search functionality added

--- Simple Weather App.py
+++ Simple Weather App.py
@@ -3,51 +3,112 @@ from PIL import ImageTk, Image
 import requests
 import json
 import reverse_geocoder as rg
-import pprint
+from io import BytesIO
+from geopy.geocoders import Nominatim
 
 
 root = Tk()
 root.title("Simple Weather")
-root.geometry("400x200")
+root.geometry("450x280")
+root.resizable(width=False, height=False)
 root.iconphoto(True, PhotoImage(file="/home/soumyo/PycharmProjects/Simple Weather Application/icon"))
 
-try:
-    api_request = requests.get("http://api.airpollutionapi.com/1.0/aqi?lat=23.40576&lon=88.49073&APPID=sfe5amidb4dsuq5uvmgf4ob4r9")
-    #api_request = requests.get("http://api.airpollutionapi.com/1.0/aqi?lat=28.7041&lon=77.1025&APPID=sfe5amidb4dsuq5uvmgf4ob4r9")
-    api = json.loads(api_request.content)
-
-    AQI = api["data"]["value"]
-    status = api["data"]["text"]
-    temp = api["data"]["temp"]
-    lat = api["data"]["coordinates"]["latitude"]
-    lon = api["data"]["coordinates"]["longitude"]
-    source = api["data"]["source"]["name"]
-    color = api["data"]["color"]
-    color = color[:-1]
-except Exception as e:
-    api = "Error ..."
-
-root.config(bg=color)
+def gridForget():
+    my_label2.grid_forget()
+    my_label1.grid_forget()
+    my_label3.grid_forget()
+    my_label4.grid_forget()
 
 
-def reverseGeocode(coordinates):
-    global city
-    result = rg.search(coordinates)
-    city = result[0]["name"]
+def search():
+    global AQI
+    global status
+    global temp
+    global lat
+    global lon
+    global color
+    global source
+    global pure_link
+    global pure_description
+    global my_label1
+    global my_label2
+    global my_label3
+    global my_label4
+    city_name = entry_box.get()
+    geolocator = Nominatim(user_agent='myapplication')
+    location = geolocator.geocode(city_name)
+    la = location.raw["lat"]
+    lo = location.raw["lon"]
+    api_add = "http://api.airpollutionapi.com/1.0/aqi?lat="+str(la)+"&lon="+str(lo)+"&APPID=sfe5amidb4dsuq5uvmgf4ob4r9"
+
+    # Using Try or Except
+    try:
+        api_request = requests.get(api_add)
+        api = json.loads(api_request.content)
+
+        AQI = api["data"]["value"]
+        status = api["data"]["text"]
+        temp = api["data"]["temp"]
+        lat = api["data"]["coordinates"]["latitude"]
+        lon = api["data"]["coordinates"]["longitude"]
+        source = api["data"]["source"]["name"]
+        color = api["data"]["color"]
+        color = color[:-1]
+        clouds = api["data"]["clouds"]
+        descripsion, link_text = clouds.split(",")
+        pure_link = link_text.split("=")
+        pure_description = descripsion.split("=")
+
+    except Exception as e:
+        api = "Error ..."
+
+    root.config(bg=color)  # Changing root window background color
+
+    url = pure_link[1]
+
+    # Creating image for weather
+    # img_url = url
+    # response = requests.get(img_url)
+    # img_data = response.content
+    # img = ImageTk.PhotoImage(Image.open(BytesIO(img_data)))
+    # img_label = Label(root, image=img, bg=color, anchor="w")
+    # img_label.grid(row=2, column=1, sticky=W)
+
+    def reverseGeocode(coordinates):
+        global city
+        result = rg.search(coordinates)
+        city = result[0]["name"]
+
+    # Driver function
+    if __name__ == "__main__":
+        # Coorinates tuple.Can contain more than one pair.
+        coordinates = (lat, lon)
+
+        reverseGeocode(coordinates)
+
+    # Creating Labels
+    my_label1 = Label(root, text="Location : " + city + "\nAQI : " + str(AQI) + " | Status : " + status, font=("Arial", 20), bg=color, fg="white")
+    my_label1.grid(row=1, column=0, columnspan=2)
+
+    my_label2 = Label(root, text="Temperature : " + temp + " °C", font=("Arial", 20), bg=color, fg="white")
+    my_label2.grid(row=3, column=0, padx=5, columnspan=2)
+
+    my_label3 = Label(root, text="Weather :" + pure_description[1], font=("Arial", 20), bg=color, fg="white")
+    my_label3.grid(row=2, column=0, padx=5, columnspan=2)
+
+    my_label4 = Label(root, text="Source : " + source, bg=color, fg="white")
+    my_label4.grid(row=4, column=0, sticky=W, pady=50, columnspan=2)
 
 
-# Driver function
-if __name__ == "__main__":
-    # Coorinates tuple.Can contain more than one pair.
-    coordinates = (lat, lon)
+# Creating EntryBox
+entry_box = Entry(root, font=("Arial", 15), width=30, bg="yellow", fg="black", justify=CENTER)
+entry_box.grid(row=0, column=0, pady=30, padx=15, sticky=W+E+N+S)
 
-    reverseGeocode(coordinates)
+# Creating Search Button
+search_btn = Button(root, text="Search", bg="grey", fg="black", font=("Arial"), command=search)
+search_btn.grid(row=0, column=1, pady=30, sticky=W+E+N+S)
 
-my_label1 = Label(root, text="Location : "+city+"\nAQI : "+str(AQI)+"\t| Status : "+status+"\nTemperature : "+temp+" °C", font=("Arial", 20), bg=color, fg="white")
-my_label1.pack(pady=30)
 
-my_label2 = Label(root, text="Source : "+source, bg=color, fg="white")
-my_label2.pack()
 
 
 
